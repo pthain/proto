e0a7d51ddb2ca91af2568fccd6440732a1e99f41
Phillip Thain <pthain@brotherhoodmutual.com>
2022-02-19 23:12:57 -0500

    Add logger tool. Write to file in log/log.txt. Add log folder to gitignore.

---
+++ lib/logger.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+##
+# This is a logger. Logs can be found at '/.../proto/log'
+class Logger
+  def initialize
+    @log_dir = File.expand_path('../log/log.txt', File.dirname(__FILE__)).to_str
+  end
+
+  def info(msg)
+    msg = format_msg(msg)
+    File.write(@log_dir, msg, mode: 'a')
+    puts(msg)
+  end
+
+  def format_msg(msg)
+    time = Time.now.getutc
+    fmsg = ''.dup
+    fmsg << time.to_s
+    fmsg << '|'
+    fmsg << 'INFO '
+    fmsg << msg
+    fmsg << "\n"
+  end
+end
--- lib/main.rb
+++ lib/main.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 
-puts('Hello World!')
+require_relative 'logger'
+
+logger = Logger.new
+
+logger.info('Application has started.')
+logger.info('Application has ended.')
